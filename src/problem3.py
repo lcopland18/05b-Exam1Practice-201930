6f3d35c553de418e355a9adc3150432e5524b059
lcopland18 <coplanlm@rose-hulman.edu>
2019-03-17 17:34:40 -0400

    Objects and Functions

--- src/problem3.py
+++ src/problem3.py
@@ -167,22 +167,16 @@ def problem3a(window, point, n):
 
     for k in range(n-1):
         line2 = rg.Line(rg.Point(line.start.x +((k+1)*20),line.start.y+(k+1)*10),rg.Point(line.start.x+((k+1)*20),line.start.y+(((k+1)*10)+50)))
-        old_thickness = line2.thickness
+        line2.thickness = line2.thickness + (k+1)*2
 
-        if line2.thickness < 13:
-            line2.thickness = line2.thickness + (k+1)*2
-            thickness_total = line2.thickness + (k+1)*2
-            print(k,line2.thickness)
-
-        else:
+        if line2.thickness > 13:
             line2.thickness = 13
-            thickness_total = line2.thickness + (k+1)*2
+            thickness_total = line2.thickness + 2
 
         line2.attach_to(window)
         window.render()
 
-
-        #return thickness_total
+    return thickness_total
 
 
 def run_test_problem3b():
