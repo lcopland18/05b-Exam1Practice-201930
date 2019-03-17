7b6465ca88447948b73eec9f2f1e886ffb712d8a
lcopland18 <coplanlm@rose-hulman.edu>
2019-03-17 16:35:24 -0400

    Objects and Functions

--- src/problem3.py
+++ src/problem3.py
@@ -3,8 +3,8 @@ PRACTICE Exam 1, problem 3.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Lauren Copland.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -104,6 +104,17 @@ def run_test_problem3a():
     # your choice), add 1 more test case of your own choosing.
     # -------------------------------------------------------------------------
 
+    # Window 3:
+    title = 'Problem 3a. Test 5: Start at (20, 20), 1 lines'
+    window4 = rg.RoseWindow(450, 300, title)
+
+    # Test 4 (it is on window 3):
+    point = rg.Point(20, 20)
+    expected = 1
+    answer = problem3a(window4, point, 1)
+    print()
+    print('Test 5 expected:', expected)
+    print('       actual:  ', answer)
 
 def problem3a(window, point, n):
     """
